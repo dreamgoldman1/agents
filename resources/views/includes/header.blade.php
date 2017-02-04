0f97acfb368801343ff60759568608c290cb3146
root <Gilberto Orozco>
2017-02-03 20:19:56 -0500

    Cambio en apuntamientos al home

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -4,7 +4,7 @@
     <div class="page-header-inner ">
         <!-- BEGIN LOGO -->
         <div class="page-logo">
-            <a href="index.html">
+            <a href="/">
                 <img src="{{ URL::asset('logo-gilberto.png') }}" alt="logo" class="logo-default" /> </a>
             <div class="menu-toggler sidebar-toggler">
                 <span></span>
